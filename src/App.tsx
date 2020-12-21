135aafb7e75c2eb47c1e683fba4ba2359a4d4feb
jacobleesinger <jacobleesinger@gmail.com>
2020-12-21 14:49:05 -0800

    [App] replace default app with dummy Step

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+import Step from './components/Step';
+import StepLeft from './components/StepLeft';
+import StepRight from './components/StepRight';
+import Input from './components/Input';
+import SubmitButton from './components/SubmitButton';
+import Result from './components/Result';
+
 function App () {
+  const [username, setUsername] = React.useState('');
+  const [usernameResult, setUsernameResult] = React.useState('');
+
+  const onUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const onSubmit = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setUsernameResult(username);
+  };
+
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <Step header='1. Enter Username'>
+        <StepLeft>
+          <form>
+            <Input
+              labelText='Username'
+              value={username}
+              onChange={onUsernameChange}
+              inputId='username'
+            />
+            <SubmitButton onClick={onSubmit} />
+          </form>
+        </StepLeft>
+        <StepRight>
+          <Result value={usernameResult} disabled={!usernameResult} label='Username' />
+        </StepRight>
+      </Step>
     </div>
   );
 }
