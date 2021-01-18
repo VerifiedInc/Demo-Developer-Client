5c84d7e221b6f22049d7f3bb78f24fa086ada2aa
Jacob Singer <jacobleesinger@gmail.com>
2021-01-18 11:52:22 -0800

    verifier sdk (#24)
    * add verifier-client-sdk
    
    * remove custom qr code and deeplink button components
    
    * [tsconfig] enable source and declaration maps
    
    * [Dockerfile/circleci] update docker build to use ssh

--- src/containers/DeeplinkStep.tsx
+++ src/containers/DeeplinkStep.tsx
@@ -6,15 +6,9 @@ import DeeplinkStep from '../components/DeeplinkStep';
 const DeeplinkStepContainer: FC = () => {
   const { presentationRequestPostResponse } = usePresentationRequestPostResponseState();
 
-  const deeplink = presentationRequestPostResponse?.deeplink;
-  const qrCode = presentationRequestPostResponse?.qrCode;
-  const isMobile = /Mobi|Android|iPhone/i.test(navigator.userAgent);
-
   return (
     <DeeplinkStep
-      deeplink={deeplink}
-      qrCodeUrl={qrCode}
-      isMobile={isMobile}
+      request={presentationRequestPostResponse}
     />
   );
 };
--- src/test/components/DeeplinkStep.test.tsx
+++ src/test/components/DeeplinkStep.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, RenderResult } from '@testing-library/react';
+import { clear as clearMockUserAgent, mockUserAgent } from 'jest-useragent-mock';
 
 import DeeplinkStep, { DeeplinkStepProps } from '../../components/DeeplinkStep';
 import { dummyPresentationRequestPostResponse } from '../mocks';
@@ -10,24 +11,23 @@ describe('Deeplink step component', () => {
 
   beforeEach(() => {
     props = {
-      deeplink: dummyPresentationRequestPostResponse.deeplink,
-      qrCodeUrl: dummyPresentationRequestPostResponse.qrCode,
-      isMobile: false
+      request: dummyPresentationRequestPostResponse
     };
 
     wrapper = render(<DeeplinkStep {...props} />);
   });
 
   it('shows a QR code on desktop', () => {
-    const img = wrapper.getByAltText('QR code');
+    const img = wrapper.getByAltText('qr code');
     expect(img).toBeInTheDocument();
   });
 
   it('shows a deeplink button on mobile', () => {
     wrapper.unmount();
-    props.isMobile = true;
+    mockUserAgent('iPhone');
     wrapper = render(<DeeplinkStep {...props} />);
-    const link = wrapper.getByText('Deep Link');
+    const link = wrapper.getByText('Continue with Unum ID Developer Demo App');
     expect(link).toBeInTheDocument();
+    clearMockUserAgent();
   });
 });
