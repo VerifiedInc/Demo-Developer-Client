047a1e4f9aaaaf993a699a4dd7d170a9eb7ca241
jacobleesinger <jacobleesinger@gmail.com>
2021-01-19 15:10:23 -0800

    update mobile styling

--- src/components/DeeplinkStep.tsx
+++ src/components/DeeplinkStep.tsx
@@ -19,7 +19,7 @@ const DeeplinkStep: FC<DeeplinkStepProps> = ({
       This is to make sure the deep link gets to the user&apos;s device,
       so it can open the correct mobile app.
       Our Verifier SDK presentat the easiest option to the user based on the situation.
-      For example, on desktop, it shows the deep link as a QR code that tue user scans.
+      For example, on desktop, it shows the deep link as a QR code that the user scans.
     </>
   );
 
