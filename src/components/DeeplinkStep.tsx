9ab26c25696092ec18511a2af325c91bffada3d3
Jacob Singer <jacobleesinger@gmail.com>
2021-03-22 14:35:29 -0700

    [package.json, DeeplinkStep] update web sdk to v0.2.2 and add deeplink button image (#42)

--- src/components/DeeplinkStep.tsx
+++ src/components/DeeplinkStep.tsx
@@ -1,17 +1,20 @@
 import React, { FC } from 'react';
-import VerifierWidget from 'verifier-client-sdk';
+import VerifierWidget from '@unumid/web-sdk';
 
 import Step from './Step';
 import StepLeft from './StepLeft';
 import { PresentationRequestPostResponse } from '../types';
 import step4Image from '../assets/step4.png';
+import buttonImage from '../assets/verify-with-app@2x.png';
 
 export interface DeeplinkStepProps {
   request?: PresentationRequestPostResponse
+  createPresentationRequest: () => Promise<any>
 }
 
 const DeeplinkStep: FC<DeeplinkStepProps> = ({
-  request = undefined
+  request = undefined,
+  createPresentationRequest
 }) => {
   const header = '4. Verifier shares deep link with subject.';
   const description = (
@@ -30,6 +33,9 @@ const DeeplinkStep: FC<DeeplinkStepProps> = ({
         <VerifierWidget
           applicationTitle='Unum ID Developer Demo'
           presentationRequest={request}
+          createPresentationRequest={createPresentationRequest}
+          createInitialPresentationRequest={false}
+          deeplinkImgSrc={buttonImage}
         />
       </StepLeft>
     </Step>
